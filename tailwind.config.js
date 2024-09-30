61198229594558c51e0a1bf0aed14b595cd20558
Astrobot <autobot268@gmail.com>
2024-09-30 21:53:56 +0530

    Font update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'trajan':['Trajan Pro',"sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
